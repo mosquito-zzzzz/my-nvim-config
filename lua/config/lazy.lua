c43c5111d1b60ba6b09bfa322410f2601d27b0d9
mosquito <oadventistas@gmail.com>
2024-07-10 00:09:46 -0300

    patches and small functionalities
    added a few functionalities and some adaptations because of the new lazyvim releases.

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -15,7 +15,6 @@ require("lazy").setup({
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
-    { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
 
